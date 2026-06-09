c6946f330958842dc399076f45f9473b70dd8829
crynta <3nnigma.dev@gmail.com>
2026-06-09 23:32:24 +0200

    feat(sidebar): animate explorer/source-control panel swap

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1032,7 +1032,7 @@ export default function App() {
                 }}
               >
                 <div className="flex h-full min-h-0 flex-col border-r border-border/60 bg-card">
-                  <div className="min-h-0 flex-1">
+                  <div key={sidebarView} className="min-h-0 flex-1 terax-panel-in">
                     {sidebarView === "explorer" ? (
                       <FileExplorer
                         ref={explorerRef}
